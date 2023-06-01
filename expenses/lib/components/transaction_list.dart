0f823238e602f649af0648465747c060acaba7fe
Efraim Kristhianno <ekriator@gmail.com>
2023-06-01 18:20:08 -0300

    update

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -32,46 +32,28 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (ctx, index) {
                 final tr = transactions[index];
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:const
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.blue,
-                          width: 2,
-                        )),
-                        padding:const EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.blue[700],
-                          ),
+                  elevation: 5,
+                  margin:const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('R\$${tr.value}'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tr.title,
-                            style:const TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(tr.date),
-                            style: TextStyle(
-                              color: Colors.blue[700],
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(tr.date),
+                    ),
                   ),
                 );
               },
--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -69,7 +69,61 @@ class _MyHomePageState extends State<MyHomePage> {
       id: 't4',
       title: 'Compras no Shopping',
       value: 592.34,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'Faxina',
+      value: 700.34,
+      date: DateTime.now().subtract(const Duration(days: 4)),
+    ),
+    Transaction(
+      id: 't6',
+      title: 'Carro de Passeio',
+      value: 62.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't7',
+      title: 'Sogra',
+      value: 93.54,
+      date: DateTime.now().subtract(const Duration(days: 2)),
+    ),
+    Transaction(
+      id: 't8',
+      title: 'Moto',
+      value: 292.34,
+      date: DateTime.now().subtract(const Duration(days: 6)),
+    ),
+    Transaction(
+      id: 't9',
+      title: 'Férias',
+      value: 792.34,
       date: DateTime.now().subtract(const Duration(days: 5)),
+    ),
+    Transaction(
+      id: 't10',
+      title: 'Compras na Turquia',
+      value: 99.34,
+      date: DateTime.now().subtract(const Duration(days: 3)),
+    ),
+    Transaction(
+      id: 't11',
+      title: 'Despesas Pessoais',
+      value: 59.34,
+      date: DateTime.now().subtract(const Duration(days: 7)),
+    ),
+    Transaction(
+      id: 't12',
+      title: 'Compras no Bar',
+      value: 92.34,
+      date: DateTime.now().subtract(const Duration(days: 9)),
+    ),
+    Transaction(
+      id: 't13',
+      title: 'Viagem de Barco',
+      value: 200.34,
+      date: DateTime.now().subtract(const Duration(days: 10)),
     ),
   ];
 
