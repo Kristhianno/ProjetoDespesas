88ba76fb13a37a375851ee2c4207a906138f89e5
Efraim Kristhianno <ekriator@gmail.com>
2023-06-01 10:26:40 -0300

    first commit

---
+++ expenses/lib/models/transaction.dart
@@ -0,0 +1,14 @@
+// Build Class
+class Transaction {
+  final String id;
+  final String title;
+  final double value;
+  final DateTime date;
+// Build Constructor
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.value,
+    required this.date,
+  });
+}
