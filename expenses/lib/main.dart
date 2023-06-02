dd7c00e7a5137c4b5b91bf4ee18a754370945df5
Efraim Kristhianno <ekriator@gmail.com>
2023-06-02 08:52:46 -0300

    update

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -161,6 +161,30 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const Drawer(
+        backgroundColor: Colors.white,
+        child: Column(
+          children: [
+            DrawerHeader(child: Icon(Icons.favorite)),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('D A S H B O A R D'),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('M E S S A G E'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('S E T T I N G S'),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('L O G O U T'),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
         actions: <Widget>[
